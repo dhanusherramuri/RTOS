bce710ad526856d05abaafcd01f4b35dbfc00285
dhanusherramuri <dhanush.erramuri.raj@gmail.com>
2026-08-08 12:42:06 +0530

    Initial Commit

---
+++ Pattern.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main (){
+
+
+// Number pattern Program
+	for( int i =0; i < 5 ; i++ ){
+
+		for(int j = 0 ; j < i+1 ; j++){
+			printf ("%d ", i+1);
+}
+printf("\n");
+}
+// * Pattern Program
+		for(int i = 0; i< 5 ; i++){
+			for(int j =0 ; j<i+1; j++){
+				printf("* ");
+}
+			printf("\n");
+}
+
+
+
+			// * Pattern 2 Program
+					printf("PATTERN 3\n");
+			for(int i = 0;i < 4 ;i++){
+				for(int j= i ;j  < 4 ; j++){
+					printf("* ");
+}
+					printf("\n");
+}
+}
